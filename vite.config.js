087a82188dc86ded2f38e6a3dde88627e40d23ac
Wakura A <baglogtnmalang@gmail.com>
2025-12-16 20:20:59 +0700

    update fitur terbaru

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,20 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: 'public/build',
+    manifest: true,
+    rollupOptions: {
+      input: {
+        app: 'resources/js/app.js',
+      },
+    },
+  },
 });
+
